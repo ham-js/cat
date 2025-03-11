7325fe90eea9a6401d5a0ba9e0c2c90ea1327062
Phillip Kessels <info@phikes.de>
2025-03-11 12:31:29 +0100

    fix: some imports

--- src/command-factories/devices/transceivers/base/types/TransceiverDevice.ts
+++ src/command-factories/devices/transceivers/base/types/TransceiverDevice.ts
@@ -1,6 +1,6 @@
 import { DeviceType } from "../../../base/enums/DeviceType"
+import { CommandFactory } from "../../../base/types/CommandFactory"
 import { Device } from "../../../base/types/Device"
-import { CommandFactory } from "../../../types/CommandFactory"
 
 // this type defines commands all transceivers need to implement. It is a generic interface to transceivers. Concrete implementations can have more yet optional keys in the params object
 type Commands = {
--- src/command-factories/devices/utils/callValidatedCommand.ts
+++ src/command-factories/devices/utils/callValidatedCommand.ts
@@ -1,3 +1,3 @@
-import { CommandFactory } from "../types/CommandFactory";
+import { CommandFactory } from "../base/types/CommandFactory";
 
 export const callValidatedCommand = <P>(command: CommandFactory<P>, params: unknown): ReturnType<typeof command> => command(command.parameterType.parse(params))
