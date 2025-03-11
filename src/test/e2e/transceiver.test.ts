cea279476decffc6b01886f22824500bb77ae7ae
Phillip Kessels <info@phikes.de>
2025-03-11 14:53:51 +0100

    chore: start e2e test

--- src/test/e2e/transceiver.test.ts
+++ src/test/e2e/transceiver.test.ts
@@ -6,19 +6,19 @@ import { clear, log } from "console"
 import { prompt } from 'enquirer'
 
 describe("end-to-end transceiver", () => {
-  beforeAll(() => clear())
+  let device: TransceiverDevice
 
-  test("works", async () => {
+  beforeAll(async () => {
+    clear()
+
     log(c.bold("⚡️ Welcome to the transceiver end-to-end test. This test will run commands against an actual device that you connect to your computer.\n"))
     log(c.bold.red("Make sure you have connected an appropriate dummy load to your transceiver!\n"))
 
-    const { confirmDummyLoad, device } = await prompt<{ confirmDummyLoad: boolean, device: TransceiverDevice }>([{
+    const { confirmDummyLoad, device: aDevice } = await prompt<{ confirmDummyLoad: boolean, device: TransceiverDevice }>([{
       choices: TransceiverDevices.map((device) => ({ name: `${device.deviceVendor} ${device.deviceName}`, value: device })),
       message: "Choose your transceiver",
       name: 'device',
-      result(value) {
-        return (this as any).map(value)
-      },
+      result(value) { return (this as any).map(value) },
       type: 'select'
     }, {
       message: c.bold.red("Have you connected an appropriate dummy load to your device?"),
@@ -28,6 +28,10 @@ describe("end-to-end transceiver", () => {
 
     if (!confirmDummyLoad) throw new Error("Need to confirm dummy load")
 
+    device = aDevice
+  })
+
+  test("works", async () => {
     expect(true).toBe(true)
   }, 10 * 60 * 1000) // 10 minutes timeout
 })
