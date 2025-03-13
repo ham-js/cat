821e7b5d4691775e0697ac27a24198c03c5217cb
Phillip Kessels <info@phikes.de>
2025-03-13 15:02:35 +0100

    chore: add type tests

--- src/devices/base/types/Device.ts
+++ src/devices/base/types/Device.ts
@@ -4,7 +4,7 @@ import { CommandFactory } from "./CommandFactory"
 import zodToJsonSchema from "zod-to-json-schema"
 
 interface DeviceWithCommand<C extends object, K extends keyof C> {
-  commands: Required<{[k in K]: C[K]}>
+  _commands: Required<{[k in K]: C[K]}>
 }
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
@@ -28,43 +28,3 @@ export abstract class Device<C extends {[k: string]: CommandFactory<any>} = {[k:
     return zodToJsonSchema(this._commands[key].parameterType)
   }
 }
-
-// const test: CommandFactory<{ p: string }> = () => "test"
-// test.parameterType = z.object({
-//   p: z.string()
-// })
-
-// abstract class SuperDevice extends Device {
-//   static deviceName = "test"
-//   static deviceType = DeviceType.Transceiver
-//   static deviceVendor = TransceiverDeviceVendor.Yaesu
-
-//   abstract commands: {
-//     test: CommandFactory<{ p: string }>
-//     abc?: CommandFactory<{ no: string }>
-//     def?: CommandFactory<{ no: string }>
-//   }
-// }
-
-// class SomeDevice extends SuperDevice {
-//   commands = {
-//     test
-//   }
-// }
-
-// const someDevice: SuperDevice = new SomeDevice()
-// someDevice.buildCommand('test', {p: "2"}) // should work
-// someDevice.buildCommand('test', {p: 2}) // shouldn't work (wrong p)
-// someDevice.buildCommand('abc', {no: "1"}) // shouldn't work - abc optional
-// someDevice.buildCommand('def', {no: "1"}) // shouldn't work - def optional
-// if (someDevice.hasCommand('abc')) someDevice.buildCommand('test', {p: "1"}) // should work - test is always safe
-// if (someDevice.hasCommand('abc')) someDevice.buildCommand('abc', {no: "1"}) // should work - abc is no guaranteed to be there
-// if (someDevice.hasCommand('abc')) someDevice.buildCommand('def', {no: "1"}) // shouldn't work - def is still not guaranteed
-// if (someDevice.hasCommand('something')) someDevice.buildCommand('something', 'never') // shouldn't work - something is not a command anytime
-
-// someDevice.getCommandSchema('def') // shouldn't work
-// someDevice.getCommandSchema('abc') // shouldn't work
-// if (someDevice.hasCommand('abc')) someDevice.getCommandSchema('abc') // should work
-// if (someDevice.hasCommand('abc')) someDevice.getCommandSchema('def') // shouldn't work
-// if (someDevice.hasCommand('abc')) someDevice.getCommandSchema('test') // should work
-// someDevice.getCommandSchema('something') // shouldn't work
---
+++ src/devices/base/types/Device.tst.ts
@@ -0,0 +1,95 @@
+import { describe, expect, test } from 'tstyche'
+import { CommandFactory } from './CommandFactory'
+import { z } from 'zod'
+import { Device } from './Device'
+import { DeviceType } from '../enums/DeviceType'
+import { TransceiverDeviceVendor } from '../../transceivers/base/types/TransceiverDeviceVendor'
+
+type NotImplementedCommand = CommandFactory<{ notImplemented: never }>
+type TestCommand<T extends string = string> = CommandFactory<{ param: T }>
+
+abstract class TestTransceiverDevice extends Device {
+  static deviceName = "test"
+  static deviceType = DeviceType.Transceiver
+  static deviceVendor = TransceiverDeviceVendor.Yaesu
+
+  abstract _commands: {
+    command: TestCommand
+    optionalImplementedCommand?: TestCommand<'optional'>
+    notImplementedCommand?: NotImplementedCommand
+  }
+}
+
+const commandParameterType  = z.object({
+  param: z.string()
+})
+const command: CommandFactory<z.infer<typeof commandParameterType>> = () => "test"
+command.parameterType = commandParameterType
+
+const optionalImplementedCommandParameterType  = z.object({
+  param: z.enum(["optional"])
+})
+const optionalImplementedCommand: CommandFactory<z.infer<typeof optionalImplementedCommandParameterType>> = () => "test"
+optionalImplementedCommand.parameterType = commandParameterType
+
+class TestDevice extends TestTransceiverDevice {
+  _commands = {
+    command,
+    optionalImplementedCommand
+  }
+}
+
+describe("Device", () => {
+  const testDevice = new TestDevice()
+  const testTransceiverDevice: TestTransceiverDevice = testDevice
+
+  test("allows to query for commands and then build them or get schema", () => {
+    // the most specific device knows out of the box what it implements
+    expect<typeof testDevice['_commands']['command']>().type.toBe<TestCommand>()
+    expect<typeof testDevice['_commands']['optionalImplementedCommand']>().type.toBe<typeof optionalImplementedCommand>()
+    expect<Parameters<typeof testDevice.buildCommand<'command'>>[1]>().type.toBe<{ param: string }>()
+    expect<Parameters<typeof testDevice.buildCommand<'optionalImplementedCommand'>>[1]>().type.toBe<{ param: 'optional' }>()
+    expect<Parameters<typeof testDevice.getCommandSchema<'command'>>[0]>().type.toBe<'command'>()
+    expect<Parameters<typeof testDevice.getCommandSchema<'optionalImplementedCommand'>>[0]>().type.toBe<'optionalImplementedCommand'>()
+
+    // in the more abstract device some commands are optional, so we can't build them or get their schema out of the box
+    expect<typeof testTransceiverDevice['_commands']['command']>().type.toBe<TestCommand>()
+    expect<typeof testTransceiverDevice['_commands']['optionalImplementedCommand']>().type.toBe<TestCommand<'optional'> | undefined>()
+    expect<Parameters<typeof testTransceiverDevice.buildCommand<'command'>>[1]>().type.toBe<{ param: string }>()
+    expect<Parameters<typeof testTransceiverDevice.buildCommand<'notImplementedCommand'>>[1]>().type.toBe<never>()
+    expect<Parameters<typeof testTransceiverDevice.buildCommand<'optionalImplementedCommand'>>[1]>().type.toBe<never>()
+    expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'command'>>[0]>().type.toBe<'command'>()
+    expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'notImplementedCommand'>>[0]>().type.toBe<never>()
+    expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'optionalImplementedCommand'>>[0]>().type.toBe<never>()
+
+    // we already know this - command is not optional
+    if (testTransceiverDevice.hasCommand('command')) {
+      expect<typeof testTransceiverDevice['_commands']['command']>().type.toBe<TestCommand>()
+      expect<Parameters<typeof testTransceiverDevice.buildCommand<'command'>>[1]>().type.toBe<{ param: string }>()
+      expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'command'>>[0]>().type.toBe<'command'>()
+
+      // this didn't change the other commands
+      expect<typeof testTransceiverDevice['_commands']['optionalImplementedCommand']>().type.toBe<TestCommand<'optional'> | undefined>()
+      expect<typeof testTransceiverDevice['_commands']['notImplementedCommand']>().type.toBe<NotImplementedCommand | undefined>()
+      expect<Parameters<typeof testTransceiverDevice.buildCommand<'notImplementedCommand'>>[1]>().type.toBe<never>()
+      expect<Parameters<typeof testTransceiverDevice.buildCommand<'optionalImplementedCommand'>>[1]>().type.toBe<never>()
+      expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'notImplementedCommand'>>[0]>().type.toBe<never>()
+      expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'optionalImplementedCommand'>>[0]>().type.toBe<never>()
+    }
+
+    // now we check the type guard for the optional command
+    if (testTransceiverDevice.hasCommand('optionalImplementedCommand')) {
+      expect<typeof testTransceiverDevice['_commands']['optionalImplementedCommand']>().type.toBe<TestCommand<'optional'>>()
+      expect<Parameters<typeof testTransceiverDevice.buildCommand<'optionalImplementedCommand'>>[1]>().type.toBe<{ param: 'optional' }>()
+      expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'optionalImplementedCommand'>>[0]>().type.toBe<'optionalImplementedCommand'>()
+
+      // this didn't change the other commands
+      expect<typeof testTransceiverDevice['_commands']['notImplementedCommand']>().type.toBe<NotImplementedCommand | undefined>()
+      expect<typeof testTransceiverDevice['_commands']['command']>().type.toBe<TestCommand>()
+      expect<Parameters<typeof testTransceiverDevice.buildCommand<'command'>>[1]>().type.toBe<{ param: string }>()
+      expect<Parameters<typeof testTransceiverDevice.buildCommand<'notImplementedCommand'>>[1]>().type.toBe<never>()
+      expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'command'>>[0]>().type.toBe<'command'>()
+      expect<Parameters<typeof testTransceiverDevice.getCommandSchema<'notImplementedCommand'>>[0]>().type.toBe<never>()
+    }
+  })
+})
