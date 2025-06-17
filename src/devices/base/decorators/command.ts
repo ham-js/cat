0bc66da457232f7393f5cabe4402fff62e560d83
Phillip Kessels <info@phikes.de>
2025-06-17 13:10:50 +0200

    chore: fix some linter warnings

--- src/devices/base/decorators/command.ts
+++ src/devices/base/decorators/command.ts
@@ -3,6 +3,7 @@ import { Device } from "../Device";
 import zodToJsonSchema from "zod-to-json-schema";
 import { JSONSchema7 } from "json-schema";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function command<This extends Device<any>, ReturnType extends Promise<unknown>>():
   (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -10,12 +11,19 @@ export function command<This extends Device<any>, ReturnType extends Promise<unk
     context: ClassMethodDecoratorContext<This>
   ) => typeof target
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function command<This extends Device<any>, SchemaShape extends z.ZodRawShape, ReturnType extends Promise<unknown>>(schemaShape: SchemaShape):
   (
     target: (this: This, parameter: z.infer<z.ZodObject<SchemaShape>>) => ReturnType,
     context: ClassMethodDecoratorContext<This>
   ) => typeof target
 
+/**
+ * Declare a method on a `Device` as a command: handles logging, input validation and introspection. 
+ * @param {object} schemaShape An object describing a zod schema (not a schema itself, but what you pass to z.object(...))
+ * @returns {Function} The decorated method
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function command<This extends Device<any>, SchemaShape extends z.ZodRawShape, ReturnType extends Promise<unknown>>(schemaShape?: SchemaShape):
   (
     target: (this: This, parameter: z.infer<z.ZodObject<SchemaShape>>) => Promise<Awaited<ReturnType>>,
